deaf6ebfd1d4b0faedbc4602dad6fc440373254d
Roma <roma.korotaev2015@mail.ru>
2021-01-24 21:10:54 +0300

    index.js PORT

--- Index.js
+++ Index.js
@@ -5,7 +5,9 @@ const mongoose = require('mongoose')
 
 const app = express()
 
-app.use(express.json({ extended: true }))
+app.use(express.json({
+  extended: true
+}))
 
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/get', require('./routes/componets.routes'))
@@ -19,7 +21,7 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
-const PORT = config.get('port') || 5000
+const PORT = config.get('port') || process.env.PORT
 
 async function start() {
   try {
